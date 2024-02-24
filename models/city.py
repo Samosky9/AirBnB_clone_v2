824bbcdedd6ea22851d1846b4d456de94bd4212b
Francaosawaru <forierie@yahoo.com>
2024-02-24 14:45:01 -0800

    task commited

--- models/city.py
+++ models/city.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
+
 """ City Module for HBNB project """
-from sqlalchemy import Column, ForeignKey, String
+
+import os
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """
-    >>City inherits from BaseModel and Base (respect the order)
-    >>class attribute __tablename__ -
-            represents the table name, cities
-    >>class attribute name
-            represents a column containing a string (128 characters)
-            cant be null
-    >>class attribute state_id
-            represents a column containing a string (60 characters)
-            cant be null
-            is a foreign key to states.id
-    """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    """ The city class, contains state ID and name """
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        from models.place import Place
+
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+
+        state_id = Column(
+                String(60),
+                ForeignKey("states.id"),
+                nullable=False)
+
+        places = relationship(
+                "Place",
+                backref="cities",
+                cascade="all, delete, delete-orphan")
+    else:
+        name = ""
+        state_id = ""
--- models/state.py
+++ models/state.py
@@ -1,12 +1,39 @@
 #!/usr/bin/python3
+
 """ State Module for HBNB project """
-from sqlalchemy import Column, ForeignKey, String
-from sqlalchemy.orm import relationship
+
+import os
 from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        from models.city import City
+
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+                "City",
+                cascade="all, delete, delete-orphan",
+                backref="state")
+
+    else:
+        name = ""
+        @property
+        def cities(self):
+            """ returns all cities with state_id == State.id """
+
+            import models
+
+            list_cities = []
+            for key, obj in models.storage.all().items():
+                if "City" in key:
+                    if obj.state_id == self.id:
+                        list_cities += [obj]
+
+            return list_cities
